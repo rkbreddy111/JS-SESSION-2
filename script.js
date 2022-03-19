4ab29ab4d050d25d5a7abea796a5f2d2a27b674e
rkbreddy111 <99252517+rkbreddy111@users.noreply.github.com>
2022-03-19 14:26:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+// Math Operation on INT and FLOAT data Types
+
+// SUM
+var sum = 10 + 15
+console.log(sum)
+
+// SUB
+var sub = 10 - 2
+console.log(sub)
+    // MUL
+var mul = 10 * 2
+console.log(mul)
+
+// DIVISION
+var div = 10 / 2
+console.log(div)
+
+// REMINDER
+
+var remain = 10 % 3
+console.log(remain)
+
+// To perform increments
+
+var sample = 10
+var sample = sample + 1
+console.log(sample)
+
+// increment a variable by one
+sample++
+console.log(sample)
+
+sample--
+console.log(sample)
+
+// increment or decrement of number by more numbers
+
+var sample1 = 10
+    // add 10 to sample1
+sample1 += 10
+console.log(sample1)
+
+sample1 -= 10
+console.log(sample1)
+
+// you can also multiply and divide different numbers
+
+sample1 *= 10
+console.log(sample1)
+
+sample1 /= 5
+console.log(sample1)
+
+
+// Some advanced math operation for INT and FLOAT
+
+var num = 3.5
+console.log(num)
+
+// Simply to round the number
+console.log(Math.round(num))
+
+// Simply to round up the number
+console.log(Math.ceil(num))
+
+// Simply to round down the number
+console.log(Math.floor(num))
+
+// -------------------------------
+
+// Working with strings
+
+var str = "Gitam University"
+
+// To find the length of the string
+console.log(str.length)
+
+// The final words of Tony Stark is "I am Iron Man"
+console.log("The final words of Tony Stark is \"I am Iron Man\"")
+console.log("This is the first line. This is the second line")
+
+
+// To make it come in 2 different line, I need to add \n
+
+console.log("This is the first line. \n this is the second line")
+
+console.log(str)
+
+// Indexing the String
+
+console.log(str[4])
+console.log(str.indexOf("t"))
+console.log(str.indexOf("University"))
+
+// -------------------------------------
+
+// Boolean 
+// Its true or false
+
+console.log(true)
+
+console.log(6 > 7)
+console.log((5 + 10) == 15)
+console.log((5 - 10) == 15)
+console.log((5 + 10) == 15)
+console.log(10 < 7)
